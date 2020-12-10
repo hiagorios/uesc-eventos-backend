249881793b5571726f95b5725a09ce113503e0b2
RenaNoel <43355816+RenaNoel@users.noreply.github.com>
2020-12-10 15:49:46 -0300

    atualização edição do usuário

--- src/main/java/br/uesc/eventos/controller/UsuarioController.java
+++ src/main/java/br/uesc/eventos/controller/UsuarioController.java
@@ -1,6 +1,8 @@
 package br.uesc.eventos.controller;
 
+import br.uesc.eventos.dto.EventoFormDTO;
 import br.uesc.eventos.dto.UsuarioFormDTO;
+import br.uesc.eventos.entity.Evento;
 import br.uesc.eventos.entity.Usuario;
 import br.uesc.eventos.repository.UsuarioRepository;
 import br.uesc.eventos.security.util.PasswordUtil;
@@ -14,6 +16,7 @@ import org.springframework.security.access.prepost.PreAuthorize;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
@@ -29,7 +32,7 @@ public class UsuarioController extends BaseController<Usuario, UsuarioRepository
     private PerfilService perfilService;
    
     @GetMapping("/formDto/{id}")
-    @Transactional
+    @PreAuthorize("hasAnyAuthority('EDITAR_USUARIO')")
     @ApiOperation(value = "Buscar DTO para edição de usuário")
     public ResponseEntity<UsuarioFormDTO> getFormDto(@PathVariable Long id) {
         Usuario usuario = service.findById(id);
@@ -45,5 +48,15 @@ public class UsuarioController extends BaseController<Usuario, UsuarioRepository
         usuario.setSenha(PasswordUtil.encode(usuario.getSenha()));
         usuario = service.create(usuario);
         return ResponseEntity.status(201).body(usuario);
+    }
+    
+    @Transactional
+    @PutMapping("/updateDto/{id}")
+    @PreAuthorize("hasAnyAuthority('EDITAR_USUARIO')")
+    @ApiOperation(value = "Enviar DTO para atualização de usuario")
+    public ResponseEntity<Void> updateDto(@PathVariable Long id, @RequestBody UsuarioFormDTO dto) {
+        Usuario usuario = service.fromFormDto(dto);
+        service.update(id, usuario);
+        return ResponseEntity.noContent().build();
     }
 }
